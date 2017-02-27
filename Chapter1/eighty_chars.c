a0865691f125b88ffd136303f663b33b1adecf08
Kevin Chesser <kevin.chesser94@gmail.com>
2017-02-27 14:22:16 -0500

    add getline func to exercise 1-17

--- Chapter1/eighty_chars.c
+++ Chapter1/eighty_chars.c
@@ -3,11 +3,29 @@
 #define MAXLINE 1000
 
 //Exercise 1-17 Print all lines that are longer than 80 characters
+
+int myGetline(char s[], int lim){
+    int c, i;
+
+    for(i = 0; i < lim - 1 && (c=getchar()) != EOF && c != '\n'; i++){
+        s[i] = c;
+    }
+    if(c == '\n'){
+        s[i] = c;
+        i++;
+    }
+        s[i] = '\0';
+        return i;
+}
+
+
+
+
 int main(void){
     int len;
     char line[MAXLINE]; 
     
-    while((len = getline(line, MAXLINE)) > 0){
+    while((len = myGetline(line, MAXLINE)) > 0){
         if(len >= MINLENGTH){
             printf("%s\n", line);
         }
